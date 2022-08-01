b34194920d95f6529301d68cc399301be849b15e
Yoshitsugu Sekine <yoshitsugu.sekine@offisis.co.jp>
2022-08-01 15:05:28 +0900

    Chapter7.9

--- app/src/main/java/jp/techacademy/yoshitsugu/apiapp/ApiApplication.kt
+++ app/src/main/java/jp/techacademy/yoshitsugu/apiapp/ApiApplication.kt
@@ -2,10 +2,17 @@ package jp.techacademy.yoshitsugu.apiapp
 
 import android.app.Application
 import io.realm.Realm
+import io.realm.RealmConfiguration
 
 class ApiApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
+        val config=RealmConfiguration.Builder()
+            .allowQueriesOnUiThread(true)
+            .allowWritesOnUiThread(true)
+            .deleteRealmIfMigrationNeeded()   //モデルクラスを変更した際にエラーが出なくなる
+            .build()
+        Realm.setDefaultConfiguration(config)
     }
 }
